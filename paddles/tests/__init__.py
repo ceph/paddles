8a361072fb60017cc16ea735c45a60bcf3491568
Alfredo Deza <alfredo@deza.pe>
2013-09-27 15:33:01 -0400

    add helpers for DB-bound tests

--- paddles/tests/__init__.py
+++ paddles/tests/__init__.py
@@ -1,21 +1,118 @@
+from copy import deepcopy
 import os
-from pecan import set_config
+from pecan import set_config, configuration
 from pecan.testing import load_test_app
+from paddles import models as pxmodel
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
-__all__ = ['FunctionalTest']
+
+def config_file():
+    here = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(here, 'config.py')
 
 
-class FunctionalTest(object):
-    """
-    Used for functional tests where you need to test your
-    literal application and its integration with the framework.
-    """
+class TestModel(object):
+
+    config = configuration.conf_from_file(config_file()).to_dict()
+    engine_url = 'sqlite://'
+
+    __db__ = None
+
+    @classmethod
+    def setup_class(cls):
+        if TestModel.__db__ is None:
+            TestModel.__db__ = 'paddlestest'
+
+            # Bind and create the database tables
+            pxmodel.clear()
+
+            db_engine = create_engine(
+                cls.engine_url,
+                encoding='utf-8',
+                poolclass=NullPool)
+
+            # AKA models.start()
+            pxmodel.Session.bind = db_engine
+            pxmodel.metadata.bind = pxmodel.Session.bind
+
+            pxmodel.Base.metadata.create_all(db_engine)
+            pxmodel.commit()
+            pxmodel.clear()
 
     def setup(self):
-        self.app = load_test_app(os.path.join(
-            os.path.dirname(__file__),
-            'config.py'
-        ))
+        config = deepcopy(self.config)
 
-    def tear_down(self):
-        set_config({}, overwrite=True)
+        # Add the appropriate connection string to the app config.
+        config['sqlalchemy'] = {
+            'url': self.engine_url,
+            'encoding': 'utf-8',
+            'poolclass': NullPool
+        }
+
+        # Set up a fake app
+        self.app = self.load_test_app(config)
+        pxmodel.start()
+
+    def load_test_app(self, config):
+        return load_test_app(config)
+
+    def teardown(self):
+        # Tear down and dispose the DB binding
+        pxmodel.clear()
+
+
+class TestApp(TestModel):
+    """
+    A controller test starts a database transaction and creates a fake
+    WSGI app.
+    """
+
+    __headers__ = {}
+
+    def _do_request(self, url, method='GET', **kwargs):
+        methods = {
+            'GET': self.app.get,
+            'POST': self.app.post,
+            'POSTJ': self.app.post_json,
+            'PUT': self.app.put,
+            'DELETE': self.app.delete
+        }
+        kwargs.setdefault('headers', {}).update(self.__headers__)
+        return methods.get(method, self.app.get)(str(url), **kwargs)
+
+    def post_json(self, url, **kwargs):
+        """
+        @param (string) url - The URL to emulate a POST request to
+        @returns (paste.fixture.TestResponse)
+        """
+        return self._do_request(url, 'POSTJ', **kwargs)
+
+    def post(self, url, **kwargs):
+        """
+        @param (string) url - The URL to emulate a POST request to
+        @returns (paste.fixture.TestResponse)
+        """
+        return self._do_request(url, 'POST', **kwargs)
+
+    def get(self, url, **kwargs):
+        """
+        @param (string) url - The URL to emulate a GET request to
+        @returns (paste.fixture.TestResponse)
+        """
+        return self._do_request(url, 'GET', **kwargs)
+
+    def put(self, url, **kwargs):
+        """
+        @param (string) url - The URL to emulate a PUT request to
+        @returns (paste.fixture.TestResponse)
+        """
+        return self._do_request(url, 'PUT', **kwargs)
+
+    def delete(self, url, **kwargs):
+        """
+        @param (string) url - The URL to emulate a DELETE request to
+        @returns (paste.fixture.TestResponse)
+        """
+        return self._do_request(url, 'DELETE', **kwargs)
+
