adbc30100466763115edf8edb62859bacad9107e
Alfredo Deza <alfredodeza@gmail.com>
2013-09-28 10:24:28 -0400

    create an errors controller

---
+++ paddles/controllers/errors.py
@@ -0,0 +1,22 @@
+from pecan import expose, request, response
+
+
+class ErrorsController(object):
+
+    @expose('json')
+    def invalid(self, **kw):
+        msg = kw.get(
+            'error_message',
+            'invalid request'
+        )
+        response.status = 400
+        return dict(message=msg)
+
+    @expose('json')
+    def not_found(self):
+        msg = kw.get(
+            'error_message',
+            'resource was not found'
+        )
+        response.status = 404
+        return dict(message=msg)
