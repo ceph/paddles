9c7e207768e3dbdfa4cc4ec77f332ddedbebd6ff
Alfredo Deza <alfredo@deza.pe>
2013-10-01 10:48:07 -0400

    add the basemetadata to alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+from paddles import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,7 +16,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
