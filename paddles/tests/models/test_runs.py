b9a347dd9261b8973fe65784a1a92f5194d038ac
Alfredo Deza <alfredo@deza.pe>
2013-10-07 12:58:44 -0400

    create tests for the new jobs count in model

---
+++ paddles/tests/models/test_runs.py
@@ -0,0 +1,14 @@
+from paddles.models import Job, Run
+from paddles.tests import TestApp
+from paddles import models
+
+
+class TestRunModel(TestApp):
+
+    def test_jobs_count(self):
+        new_run = Run('some-run-here')
+        Job({}, new_run)
+        Job({}, new_run)
+        models.commit()
+        run_as_json = Run.get(1).__json__()
+        assert run_as_json['jobs_count'] == 2
