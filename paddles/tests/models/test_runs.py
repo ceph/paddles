b45f57dd5027560a61a23e937890043ed1e99397
Alfredo Deza <alfredo@deza.pe>
2013-10-11 11:23:15 -0400

    fix last broken polluted test

--- paddles/tests/models/test_runs.py
+++ paddles/tests/models/test_runs.py
@@ -6,6 +6,8 @@ from paddles import models
 class TestRunModel(TestApp):
 
     def test_jobs_count(self):
+        Run.query.delete()
+        Job.query.delete()
         new_run = Run('some-run-here')
         Job({}, new_run)
         Job({}, new_run)
