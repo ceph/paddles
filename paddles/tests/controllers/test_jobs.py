2b349e9f00e823aa393e33ce1d17a3755d703e87
Alfredo Deza <alfredo@deza.pe>
2013-10-11 10:44:16 -0400

    add tests to find job update bug

--- paddles/tests/controllers/test_jobs.py
+++ paddles/tests/controllers/test_jobs.py
@@ -15,3 +15,50 @@ class TestJobsController(TestApp):
         response = self.app.get('/runs/foo/jobs/')
         assert len(response.json) == 1
         assert response.json[0]['job_id'] == '1'
+
+    def test_attempt_to_create_a_new_job(self):
+        self.app.post_json('/runs/', dict(name="foo"))
+        self.app.post_json('/runs/foo/jobs/', dict(
+            job_id=1,
+        ))
+        response = self.app.post_json('/runs/foo/jobs/', dict(), expect_errors=True)
+        assert response.status_code == 400
+
+    def test_to_create_a_new_job(self):
+        self.app.post_json('/runs/', dict(name="foo"))
+        self.app.post_json('/runs/foo/jobs/', dict(
+            job_id=1,
+        ))
+        response = self.app.post_json(
+            '/runs/foo/jobs/',
+            dict(job_id=1234),
+        )
+        assert response.status_code == 200
+
+    def test_to_get_newly_created_job(self):
+        self.app.post_json('/runs/', dict(name="foo"))
+        self.app.post_json('/runs/foo/jobs/', dict(
+            job_id=1,
+        ))
+        self.app.post_json(
+            '/runs/foo/jobs/',
+            dict(job_id=1234),
+        )
+        response = self.app.get('/runs/foo/jobs/1234/')
+        assert response.status_code == 200
+
+    def test_update_a_job(self):
+        self.app.post_json('/runs/', dict(name="foo"))
+        self.app.post_json('/runs/foo/jobs/', dict(
+            job_id=1,
+        ))
+        self.app.post_json(
+            '/runs/foo/jobs/',
+            dict(job_id=1234),
+        )
+        response = self.app.put_json(
+            '/runs/foo/jobs/1234/',
+            dict(job_id=1234),
+        )
+        assert response.status_code == 200
+
