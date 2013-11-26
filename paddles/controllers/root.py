442210a4f45d4236252a73dc2af14c8da50b43d7
Zack Cerza <zack@cerza.org>
2013-11-26 13:25:50 -0600

    Reflect new GitHub location

--- paddles/controllers/root.py
+++ paddles/controllers/root.py
@@ -9,7 +9,7 @@ from paddles.controllers.util import last_seen
 class RootController(object):
 
     _help = {
-        "docs": "https://github.com/alfredodeza/paddles",
+        "docs": "https://github.com/ceph/paddles",
         "extensions": {
             "Chrome": "https://chrome.google.com/webstore/detail/jsonview/chklaanhfefbnpoihckbnefhakgolnmc",
             "Firefox" : "https://addons.mozilla.org/en-US/firefox/addon/jsonview/"
