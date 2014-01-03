f1dbb57c60bb0b41f0477a8f3bb496dff944a7c8
Zack Cerza <zack@cerza.org>
2014-01-03 12:55:16 -0600

    User Dozer.

--- paddles/app.py
+++ paddles/app.py
@@ -1,3 +1,4 @@
+from dozer import Dozer
 from pecan import make_app
 from paddles import models
 
@@ -7,8 +8,9 @@ def setup_app(config):
     models.init_model()
     app_conf = dict(config.app)
 
-    return make_app(
+    app = make_app(
         app_conf.pop('root'),
         logging=getattr(config, 'logging', {}),
         **app_conf
     )
+    return Dozer(app)
