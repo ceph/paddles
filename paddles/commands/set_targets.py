5dae5955a29e41742ac1a41a9aaad636d49a87f8
Zack Cerza <zack@cerza.org>
2014-03-26 12:42:16 -0500

    Improve set_targets command
    When it's rerun, it will skip jobs it's processed before. Also, it will
    make sure nodes have their machine_type field set.

--- paddles/commands/set_targets.py
+++ paddles/commands/set_targets.py
@@ -1,6 +1,6 @@
 from pecan.commands.base import BaseCommand
 
-from paddles import models
+from paddles.models import start, rollback, commit, Job
 from paddles.models import Node
 
 
@@ -19,17 +19,21 @@ class SetTargetsCommand(BaseCommand):
         self.load_app()
         try:
             out("STARTING A TRANSACTION...")
-            models.start()
-            jobs = models.Job.query.all()
+            start()
+            jobs = Job.query.filter(~Job.target_nodes.any()).all()
             for job in jobs:
                 self._populate(job)
+
+            nodes = Node.query.filter(Node.machine_type.is_(None)).all()
+            for node in nodes:
+                node.machine_type = self.parse_machine_type(node.name)
         except:
-            models.rollback()
+            rollback()
             out("ROLLING BACK... ")
             raise
         else:
             out("COMMITING... ")
-            models.commit()
+            commit()
 
     def _populate(self, job):
         print "Job: %s/%s" % (job.name, job.job_id)
@@ -38,6 +42,7 @@ class SetTargetsCommand(BaseCommand):
 
         for key in job.targets.keys():
             name = key.split('@')[1]
+            mtype = self.parse_machine_type(name)
             node_q = Node.query.filter(Node.name == name)
             print " node: exists={count}, name={name}".format(
                 count=node_q.count(),
@@ -48,5 +53,16 @@ class SetTargetsCommand(BaseCommand):
                 node = Node(name=name)
             else:
                 node = node_q.one()
+            if mtype:
+                node.machine_type = mtype
             if node not in job.target_nodes:
                 job.target_nodes.append(node)
+
+    @staticmethod
+    def parse_machine_type(node_name):
+        types = Node.machine_types
+        if node_name.startswith('vpm') and 'vps' in types:
+            return 'vps'
+        for mtype in types:
+            if node_name.startswith(mtype):
+                return mtype
