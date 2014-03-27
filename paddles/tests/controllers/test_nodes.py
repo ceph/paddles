2ef6e6aacfc14b77aeab1154138751db609fd453
Zack Cerza <zack@cerza.org>
2014-03-27 16:39:57 -0500

    Add NodeController test: /nodes/$NODE/job_stats/

--- paddles/tests/controllers/test_nodes.py
+++ paddles/tests/controllers/test_nodes.py
@@ -36,3 +36,22 @@ class TestNodesController(TestApp):
             result[name] = dict(fail=len(job_ids))
         response = self.app.get('/nodes/job_stats/')
         assert response.json == result
+
+
+class TestNodeController(TestApp):
+
+    def test_single_node_job_stats(self):
+        run_name = 'job_stats'
+        job_ids = [1, 2, 3]
+        target_name = 't1'
+        targets = {'u@' + target_name: ''}
+        for job_id in job_ids:
+            self.app.post_json('/runs/{name}/jobs/'.format(name=run_name),
+                               dict(job_id=job_id, targets=targets,
+                                    status='running'))
+
+        result = {'running': len(job_ids), 'pass': 0, 'fail': 0, 'dead': 0,
+                  'unknown': 0}
+        response = self.app.get('/nodes/{node}/job_stats/'.format(
+            node=target_name))
+        assert response.json == result
