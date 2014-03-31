1a18952f26f58bc53031fae78aa5a38d1e1db8fb
Zack Cerza <zack@cerza.org>
2014-03-31 11:37:46 -0500

    Add lots more tests for error handling

--- paddles/tests/controllers/test_nodes.py
+++ paddles/tests/controllers/test_nodes.py
@@ -3,6 +3,10 @@ from paddles.tests import TestApp
 
 class TestNodesController(TestApp):
 
+    def test_get_node_root(self):
+        response = self.app.get('/nodes/')
+        assert response.status_int == 200
+
     def test_job_creates_nodes(self):
         run_name = 'job_creates_nodes'
         job_id = 276
@@ -45,8 +49,22 @@ class TestNodesController(TestApp):
         response = self.app.get('/nodes/{name}/'.format(name=node_name))
         assert response.json['name'] == node_name
 
+    def test_get_nodes_invalid_machine_type(self):
+        response = self.app.get('/nodes/?machine_type=not_a_thing',
+                                expect_errors=True)
+        assert response.status_int == 404
+
+    def test_job_stats_invalid_machine_type(self):
+        response = self.app.get('/nodes/job_stats?machine_type=not_a_thing',
+                                expect_errors=True)
+        assert response.status_int == 404
+
 
 class TestNodeController(TestApp):
+
+    def test_get_nonexistent_node(self):
+        response = self.app.get('/nodes/this_is_not_here/', expect_errors=True)
+        assert response.status_int == 404
 
     def test_single_node_job_stats(self):
         run_name = 'job_stats'
@@ -71,3 +89,35 @@ class TestNodeController(TestApp):
                           dict(name=node_name, locked=True))
         response = self.app.get('/nodes/{name}/'.format(name=node_name))
         assert response.json['locked'] is True
+
+    def test_post_junk(self):
+        response = self.app.post_json('/nodes/', dict(), expect_errors=True)
+        assert response.status_int == 400
+
+    def test_post_empty_name(self):
+        response = self.app.post_json('/nodes/', dict(name=None),
+                                      expect_errors=True)
+        assert response.status_int == 400
+
+    def test_post_dupe(self):
+        node_dict = dict(name='post_me_twice')
+        response = self.app.post_json('/nodes/', node_dict)
+        response = self.app.post_json('/nodes/', node_dict, expect_errors=True)
+        assert response.status_int == 400
+
+    def test_update_nonexistent(self):
+        node_name = 'missing_kitten'
+        response = self.app.put_json('/nodes/{name}/'.format(name=node_name),
+                                     dict(name=node_name, locked=True),
+                                     expect_errors=True)
+        assert response.status_int == 404
+
+    def test_jobs_nonexistent(self):
+        response = self.app.get('/nodes/missing_kitten/jobs/',
+                                expect_errors=True)
+        assert response.status_int == 404
+
+    def test_job_stats_nonexistent(self):
+        response = self.app.get('/nodes/missing_kitten/job_stats/',
+                                expect_errors=True)
+        assert response.status_int == 404
