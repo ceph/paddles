2dce268a1f1243356ec991a7e526e240b2d1bf9a
Zack Cerza <zack@cerza.org>
2014-06-03 10:33:41 -0500

    Add command for printing queue stats
    Currently prints how many jobs were scheduled vs. completed (pass or
    fail) on a particular day. It was created to help diagnose issues with
    test throughput.
    It takes an int, days, for how many days to look back in history.
    
    Signed-off-by: Zack Cerza <zack.cerza@inktank.com>

---
+++ paddles/commands/queue_stats.py
@@ -0,0 +1,60 @@
+from pecan.commands.base import BaseCommand
+
+from collections import OrderedDict
+from datetime import date
+from paddles import models
+from paddles.models import Run, Job
+
+
+def out(string):
+    print "==> %s" % string
+
+
+class QueueStatsCommand(BaseCommand):
+    """
+    Print the number of jobs scheduled versus completed (passed or failed) for each of N days in the past
+    """
+
+    arguments = BaseCommand.arguments + (dict(
+        name="days",
+        help="How many days to go back in history",
+    ),)
+
+    def run(self, args):
+        super(QueueStatsCommand, self).run(args)
+        days = int(args.days) + 1
+        self.load_app()
+        models.start()
+        today = date.today()
+        day_objs = []
+        for day_num in range(days)[::-1]:
+            day = date.fromordinal(today.toordinal() - day_num)
+            day_objs.append(day)
+
+        job_counts = {}
+        for day in day_objs:
+            if day_objs[-1] == day:
+                break
+            next_day = day_objs[day_objs.index(day) + 1]
+            jobs_sched = self.jobs_scheduled_between(day, next_day).count()
+            jobs_done = self.jobs_completed_between(day, next_day).count()
+            job_counts[day] = OrderedDict(scheduled=jobs_sched,
+                                          completed=jobs_done)
+            print "{day}: {sched: >4} scheduled, {done: >4} completed".format(
+                day=day,
+                sched=jobs_sched,
+                done=jobs_done,
+            )
+
+    def jobs_scheduled_between(self, day1, day2):
+        query = Run.query.filter(Run.scheduled.between(day1, day2))
+        query = query.join(Job)
+        return query
+
+    def jobs_completed_between(self, day1, day2):
+        statuses = ['pass', 'fail']
+        query = self.jobs_scheduled_between(day1, day2)
+        query = query.filter(Job.status.in_(statuses))
+        return query
+
+
--- setup.py
+++ setup.py
@@ -27,5 +27,6 @@ setup(
         set_status=paddles.commands.set_status:SetStatusCommand
         set_targets=paddles.commands.set_targets:SetTargetsCommand
         import_nodes=paddles.commands.import_nodes:ImportNodesCommand
+        queue_stats=paddles.commands.queue_stats:QueueStatsCommand
         """
 )
