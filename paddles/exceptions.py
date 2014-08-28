1745e99e360a8885ba5843e5885db9ef4033b77d
Zack Cerza <zack.cerza@inktank.com>
2014-08-28 08:34:44 -0600

    Add exceptions module
    Signed-off-by: Zack Cerza <zack.cerza@inktank.com>

---
+++ paddles/exceptions.py
@@ -0,0 +1,19 @@
+
+class PaddlesError(StandardError):
+    pass
+
+
+class InvalidRequestError(PaddlesError, ValueError):
+    url = '/errors/invalid/'
+
+
+class ForbiddenRequestError(PaddlesError, RuntimeError):
+    url = '/errors/forbidden/'
+
+
+class ResourceNotFoundError(PaddlesError, ValueError):
+    url = '/errors/not_found/'
+
+
+class ResourceUnavailableError(PaddlesError, RuntimeError):
+    url = '/errors/unavailable/'
