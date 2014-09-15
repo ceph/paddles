1a574911dc24bd3d36dc804c70e43b0f1dced1cc
Zack Cerza <zack.cerza@inktank.com>
2014-09-15 09:45:07 -0600

    expire

---
+++ paddles/commands/expire_jobs.py
@@ -0,0 +1,18 @@
+from pecan.commands.base import BaseCommand
+
+from paddles import models
+from paddles.models import Job
+
+from datetime import datetime, timedelta
+
+
+class ExpireJobsCommand(BaseCommand):
+    def run(self, args):
+        super(ExpireJobsCommand, self).run(args)
+        self.load_app()
+        models.start()
+        delta = timedelta(seconds=60*60*6)
+        now = datetime.utcnow()
+        running = Job.query.filter(Job.status == 'running')
+        to_expire = running.filter(~Job.updated.between(now - delta, now))
+        print to_expire.count()
