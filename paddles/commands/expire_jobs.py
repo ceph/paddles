2ec75b68a57917475bb07673c6d63981226b65d7
Zack Cerza <zack.cerza@inktank.com>
2014-09-17 10:16:07 -0600

    Drop expire time to 30m; also set run status
    Signed-off-by: Zack Cerza <zack.cerza@inktank.com>

--- paddles/commands/expire_jobs.py
+++ paddles/commands/expire_jobs.py
@@ -14,14 +14,18 @@ class ExpireJobsCommand(BaseCommand):
         super(ExpireJobsCommand, self).run(args)
         self.load_app()
         models.start()
-        delta = timedelta(seconds=60*60*6)
+        delta = timedelta(seconds=60*60*0.5)
         now = datetime.utcnow()
         running = Job.query.filter(Job.status == 'running')
         to_expire = running.filter(~Job.updated.between(now - delta, now))
         msg = "Expiring {count} jobs".format(count=to_expire.count())
         print msg
+        runs = set()
         for job in to_expire:
             job.status = 'dead'
+            runs.add(job.run)
+        for run in runs:
+            run.set_status()
         try:
             models.commit()
         except:
