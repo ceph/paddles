28548110a3f107e2e5ef206af89d5a80ce2b07e6
Aishwarya Mathuria <amathuri@redhat.com>
2021-07-23 19:33:45 +0530

    alembic: add sequence creation to upgrade script
    Signed-off-by: Aishwarya Mathuria <amathuri@redhat.com>

--- alembic/versions/e8de4928657_modify_jobs_table.py
+++ alembic/versions/e8de4928657_modify_jobs_table.py
@@ -12,6 +12,7 @@ down_revision = '4699efaaee99'
 
 from alembic import op
 from paddles.models.types import JSONType
+from sqlalchemy.schema import Sequence, CreateSequence, DropSequence
 
 import sqlalchemy as sa
 
@@ -30,6 +31,7 @@ def upgrade():
     op.add_column(u'jobs', sa.Column('user', sa.String(length=64), nullable=True))
     op.create_index(op.f('ix_jobs_job_id'), 'jobs', ['job_id'], unique=False)
     op.create_index(op.f('ix_jobs_teuthology_sha1'), 'jobs', ['teuthology_sha1'], unique=False)
+    op.execute(CreateSequence(Sequence('job_id_sequence')))
     op.drop_index('ix_jobs_archive_path', table_name='jobs')
 
 
@@ -37,6 +39,7 @@ def downgrade():
     op.create_index('ix_jobs_archive_path', 'jobs', ['archive_path'], unique=False)
     op.drop_index(op.f('ix_jobs_teuthology_sha1'), table_name='jobs')
     op.drop_index(op.f('ix_jobs_job_id'), table_name='jobs')
+    op.execute(DropSequence(Sequence('job_id_sequence')))
     op.drop_column(u'jobs', 'user')
     op.drop_column(u'jobs', 'timestamp')
     op.drop_column(u'jobs', 'teuthology_sha1')
