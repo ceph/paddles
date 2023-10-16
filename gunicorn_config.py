f421266569feecee1c0d66605b511bdf4a36396e
Zack Cerza <zack@redhat.com>
2023-10-16 13:27:19 -0600

    gunicorn_config.py: Don't log to files by default
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,9 @@
-import os
+# import os
 import multiprocessing
 
 workers = multiprocessing.cpu_count()
 max_requests = 10000
 timeout = 60
 # loglevel = 'debug'
-accesslog = os.path.expanduser("~/paddles.access.log")
+# accesslog = os.path.expanduser("~/paddles.access.log")
 # errorlog = os.path.expanduser("~/paddles.error.log")
