ceph/paddles

commit fb1c596bd68a559bb504a80dce39966cc6578c45

Node controllers: Don't query the entire node obj
Assembling these return values cause lots of extra db reads, and we
aren't using the values anywhere I can see.

Signed-off-by: Zack Cerza <zack@redhat.com>

paddles/controllers/nodes.py
@@ -320,7 +320,11 @@ class NodeController(object):
             ))
         except PaddlesError as exc:
             error(exc.url, str(exc))
-        return node_obj.__json__()
+        return dict(
+            name=node_obj.name,
+            locked=node_obj.locked,
+            locked_by=node_obj.locked_by,
+        )
 
     @expose('json')
     def jobs(self, name='', status='', count=0, page=1):

paddles/tests/controllers/test_nodes.py
@@ -362,6 +362,7 @@ class TestNodeController(TestApp):
         response = self.app.put_json(
             '/nodes/{name}/lock'.format(name=node_name),
             dict(locked=False, locked_by='me'))
+        assert response.status_int == 200
         assert response.json['locked'] is False and \
             response.json['locked_by'] is None
 

commit ad9afcdcea65718c14d5b776861648bfa94a3cfc

controllers/nodes: add missing records for return objects
On teuthology side at least for vms the lock/unlock mechanism still
needs some records in return object like machine_type and is_vm.

Fixes: fb1c596bd68a559bb504a80dce39966cc6578c45
Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@clyso.com>

paddles/controllers/nodes.py
@@ -314,6 +314,8 @@ class NodeController(object):
             name=node_obj.name,
             locked=node_obj.locked,
             locked_by=node_obj.locked_by,
+            machine_type=node_obj.machine_type,
+            is_vm=node_obj.is_vm,
         )
 
     @expose('json')